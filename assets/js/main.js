aed9b6233935c9dd3368779d4bfd799b4f5b6cb8
George Mcalpine <georgemcalpine@Georges-iMac.local>
2022-01-29 11:11:56 -0500

    converted date format

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ var getWeather = function(searchCity) {
     // format the github api url
     //var apiUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + searchCity + "&cnt=5&APPID=80ef5c6717f3834714ead7f302cc767c";
     //var apiUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Chicago&cnt=5&APPID=80ef5c6717f3834714ead7f302cc767c";
-    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=80ef5c6717f3834714ead7f302cc767c";
+    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&units=imperial&appid=80ef5c6717f3834714ead7f302cc767c";
     
     // make a get request to url
     fetch(apiUrl)
@@ -23,7 +23,8 @@ var getWeather = function(searchCity) {
           console.log(response);
           response.json().then(function(data) {
             console.log(data);
-            //displayRepos(data, user);
+            //displayWeather(data);
+            storeSearch(searchCity);
           });
         } else {
           alert("Error: the city searched for is " + response.statusText);
@@ -36,15 +37,45 @@ var getWeather = function(searchCity) {
 
   var displayWeather = function(data) {
     /* display the weather and forecast for searchCity */
+    console.log(data.city.name);
+    console.log(data.cnt);
+    for (var i=0; i< data.cnt; i++) {
+      var dttm = convertDate(data.list[i].dt);
+      var temp = data.list[i].main.temp;
+      var minT = data.list[i].main.temp_min;
+      var maxT = data.list[i].main.temp_max;
+      var descr = data.list[i].weather[0].main;
+      var wind = data.list[i].wind.speed;
+      console.log(dttm + ',' + temp + ',' + minT + ',' + maxT + ',' + descr + ',' + wind);
+    }
   }
 
-  var storeSearch = function() {
+  var storeSearch = function(searchCity) {
     /* store in localstorage */
+    const searchHist = {
+      cityName: searchCity
+    };
+    window.localStorage.setItem("city", JSON.stringify(searchHist));
   }
 
   var getPreviousSearches = function() {
     /* get previous searches from local storage and display under search input */
+    var prevSearch = JSON.parse(window.localStorage.getItem("city"));
+    console.log("previous: " + prevSearch);
   }
   
+  var convertDate = function(date) {
+    let timestamp = date;
+    // Create a new JavaScript Date object based on the timestamp
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    var uFormDate = new Date(timestamp * 1000);
+    var month = uFormDate.getMonth() + 1;
+    var date = uFormDate.getDate();
+    var year = uFormDate.getFullYear();
+    // Will display date in MM/DD/YYYY formate
+    var formattedDate = month + '\/' + date + '\/' + year;
+    return formattedDate;
+  }
   
+  getPreviousSearches();
   searchButton.addEventListener("click", getCity);
