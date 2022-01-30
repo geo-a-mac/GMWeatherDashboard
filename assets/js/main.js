78db01cd560b31f90df6030e59ae79cd11584bfb
George Mcalpine <georgemcalpine@Georges-iMac.local>
2022-01-30 13:48:20 -0500

    display 5 day forecast

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,31 @@ var searchButton = document.getElementById("searchBtn");
 var searchList = document.querySelector("#searchList");
 var APIKey = "80ef5c6717f3834714ead7f302cc767c";
 var searchHist = ["No previous search"];
+// vars for displaying weather data
+var cityDate = document.getElementById("cityDate");
+var todayTemp = document.getElementById("temp");
+var todayWind = document.getElementById("wind");
+var todayHum = document.getElementById("humidity");
+var date1 = document.getElementById("b1");
+var temp1 = document.getElementById("temp1");
+var wind1 = document.getElementById("wind1");
+var hum1 = document.getElementById("hum1");
+var date2 = document.getElementById("b2");
+var temp2 = document.getElementById("temp2");
+var wind2 = document.getElementById("wind2");
+var hum2 = document.getElementById("hum2");
+var date3 = document.getElementById("b3");
+var temp3 = document.getElementById("temp3");
+var wind3 = document.getElementById("wind3");
+var hum3 = document.getElementById("hum3");
+var date4 = document.getElementById("b4");
+var temp4 = document.getElementById("temp4");
+var wind4 = document.getElementById("wind4");
+var hum4 = document.getElementById("hum4");
+var date5 = document.getElementById("b5");
+var temp5 = document.getElementById("temp5");
+var wind5 = document.getElementById("wind5");
+var hum5 = document.getElementById("hum5");
 
 var histSearch = function (event) {
   console.log("histSearch");
@@ -27,42 +52,76 @@ var getCity = function() {
 }
 
 var getWeather = function(searchCity) {
-    // format the openweather api url
-    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&units=imperial&appid=80ef5c6717f3834714ead7f302cc767c";
-    
-    // make a get request to url
-    fetch(apiUrl)
-      .then(function(response) {
-        // request was successful
-        if (response.ok) {
-          console.log(response);
-          response.json().then(function(data) {
-            console.log(data);
-            //displayWeather(data);
-            storeSearch(searchCity);
-          });
-        } else {
-          alert("Error: the city searched for is " + response.statusText);
-        }
-      })
-      .catch(function(error) {
-        alert("Unable to connect to GitHub");
-      });
-  };
+  // format the openweather api url
+  var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&units=imperial&appid=80ef5c6717f3834714ead7f302cc767c";
+  
+  // make a get request to url
+  fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function(data) {
+          console.log(data);
+          displayWeather(data);
+          storeSearch(searchCity);
+        });
+      } else {
+        alert("Error: the city searched for is " + response.statusText);
+      }
+    })
+    .catch(function(error) {
+      alert("Unable to connect");
+    });
+};
 
   var displayWeather = function(data) {
     /* display the weather and forecast for searchCity */
     console.log(data.city.name);
-    console.log(data.cnt);
-    for (var i=0; i< data.cnt; i++) {
+    var cName = data.city.name;
+    for (var i=0; i<data.cnt; i+=8) {
       var dttm = convertDate(data.list[i].dt);
       var temp = data.list[i].main.temp;
-      var minT = data.list[i].main.temp_min;
-      var maxT = data.list[i].main.temp_max;
       var descr = data.list[i].weather[0].main;
       var wind = data.list[i].wind.speed;
-      console.log(dttm + ',' + temp + ',' + minT + ',' + maxT + ',' + descr + ',' + wind);
+      var humid = data.list[i].main.humidity;
+          console.log(dttm + ',' + temp + ',' + descr + ',' + wind + ',' + humid);
+      if (i === 0) {
+        cityDate.textContent = cName + " " + dttm + " " + descr;
+        todayTemp.textContent = "Temperature: " + temp + "F";
+        todayWind.textContent = "Wind speed: " + wind + "mph";
+        todayHum.textContent = "Humidity: " + humid + "%";
+      } else if (i === 8) {
+        date1.textContent = "Date: " + dttm;
+        temp1.textContent = "Temperature: " + temp + "F"; 
+        wind1.textContent = "Wind speed: " + wind + "mph";
+        hum1.textContent = "Humidity: " + humid + "%";
+      } else if (i === 16) {
+        date2.textContent = "Date: " + dttm;
+        temp2.textContent = "Temperature: " + temp + "F";
+        wind2.textContent = "Wind speed: " + wind + "mph";
+        hum2.textContent = "Humidity: " + humid + "%";
+      } else if (i === 24) {
+        date3.textContent = "Date: " + dttm;
+        temp3.textContent = "Temperature: " + temp + "F";
+        wind3.textContent = "Wind speed: " + wind + "mph";
+        hum3.textContent = "Humidity: " + humid + "%";
+      } else if (i === 32) {
+        date4.textContent = "Date: " + dttm;
+        temp4.textContent = "Temperature: " + temp + "F";
+        wind4.textContent = "Wind speed: " + wind + "mph";
+        hum4.textContent = "Humidity: " + humid + "%";
+      } 
     }
+    var dttm = convertDate(data.list[39].dt);
+    var temp = data.list[39].main.temp;
+    var descr = data.list[39].weather[0].main;
+    var wind = data.list[39].wind.speed;
+    var humid = data.list[39].main.humidity;
+    date5.textContent = "Date: " + dttm;
+    temp5.textContent = "Temperature: " + temp + "F";
+    wind5.textContent = "Wind speed: " + wind + "mph";
+    hum5.textContent = "Humidity: " + humid + "%";
   }
 
   var storeSearch = function(searchCity) {
